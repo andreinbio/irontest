d92aaffc4333f02a96350e78b5bed803a68306fe
Andrei Nagacevschi <andrei.nagacevschi@osf-commerce.com>
2017-08-13 20:16:32 +0300

    Adding external library for parsing files.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate iron;
 extern crate router;
+extern crate rustview;
+
+use rustview::view::View;
 
 use iron::prelude::*;
 use iron::status;
@@ -7,15 +10,18 @@ use iron::headers::{ContentType};
 use router::Router;
 
 fn handler(req: &mut Request) -> IronResult<Response> {
-    let mut response = Response::with((status::Ok, "<strong>Test</strong> <sup>this</sup>"));
+    let template = View::new();
+    let mut response = Response::with((status::Ok, template.render("index.html")));
     response.headers.set(ContentType::html());
     Ok(response)
+
 }
 
 fn main() {
-    let mut router = Router::new();           // Alternative syntax:
-    router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
-    router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
+    let mut router = Router::new();
+    router.get("/", handler, "index");
+    router.get("/:query", handler, "query");
 
+    println!("Server running at http://localhost:3000");
     Iron::new(router).http("localhost:3000").unwrap();
 }
